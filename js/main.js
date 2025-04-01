eb5802dd9eaac5a9f8b8f47233629d8f1849dc4f
chanwinharold <chanwinharold@gmail.com>
2025-04-01 14:16:57 +0200

    Refactoring et Correction du code

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@ const regexNames = new RegExp("^[A-Za-z]+$")
 const regexEmail = new RegExp("^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z0-9]+$")
 const regexPassword = new RegExp("^.{8,}$")
 
-document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault()
+let allInputAreValid = true;
 
-    document.querySelectorAll("input").forEach((input) => {
+document.querySelector("form").addEventListener("submit", (event) => {
+    if (!allInputAreValid) {
+        event.preventDefault()
+    } else {
+        alert("✅ Formulaire envoyé avec succès !")
+    }
+})
+
+document.querySelectorAll("input").forEach((input) => {
+    input.addEventListener("focusout", () => {
         if (input.value.trim() === "") {
             addError(input, `${input.placeholder} cannot be empty`)
+            allInputAreValid = false
         } else {
             if (["name", "last_name"].includes(input.name) && !(regexNames.test(input.value))) {
                 addError(input, `Please enter a valid ${input.placeholder}`)
+                allInputAreValid = false
             }
             if (input.name === "email" && !(regexEmail.test(input.value))) {
                 addError(input, "Looks like this is not an email")
+                allInputAreValid = false
             }
             if (input.name === "password" && !(regexPassword.test(input.value))) {
                 addError(input, "Password should contain at least 8 characters")
+                allInputAreValid = false
             }
         }
     })
@@ -26,6 +38,7 @@ document.querySelectorAll("input").forEach((input) => {
     input.addEventListener("input", () => {
         if (input.value.trim() !== "") {
             removeError(input)
+            allInputAreValid = true
         }
     })
 })
@@ -33,13 +46,13 @@ document.querySelectorAll("input").forEach((input) => {
 
 function addError(input, message) {
     document.querySelector(`input[name="${input.name}"] + span`).textContent = message
-    document.querySelector(`.${input.name}`).src = "images/icon-error.svg"
+    document.querySelector(`.${input.name}`).innerHTML = `<img src="/images/icon-error.svg" alt="error">`
     input.style.borderColor = "hsl(0, 100%, 74%)"
     input.style.color = "hsl(0, 100%, 74%)"
 }
 function removeError(input) {
     document.querySelector(`input[name="${input.name}"] + span`).textContent = ""
-    document.querySelector(`.${input.name}`).src = ""
+    document.querySelector(`.${input.name}`).innerHTML = ""
     input.style.borderColor = "hsl(246, 25%, 77%)"
     input.style.color = "black"
 }
